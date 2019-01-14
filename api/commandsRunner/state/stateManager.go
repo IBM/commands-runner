925d1b6f6bdc8d9bc80396002abcff5e1e449116
Dominique Vernier <dvernier@Dominiques-MacBook-Pro.local>
2019-01-14 03:50:04 -0500

    Add PREPROCESSING state

--- api/commandsRunner/state/stateManager.go
+++ api/commandsRunner/state/stateManager.go
@@ -58,6 +58,7 @@ const StateFAILED = "FAILED"
 const StateSUCCEEDED = "SUCCEEDED"
 const StateRUNNING = "RUNNING"
 const StateSKIP = "SKIP"
+const StatePREPROCESSING = "PREPROCESSING"
 
 const StatesFileErrorMessagePattern = "STATES_FILE_ERROR_MESSAGE:"
 
@@ -1810,6 +1811,7 @@ func (sm *States) setStatesExecutionID(callerState *State) error {
 		sm.ExecutedByExtensionName = callerState.ExecutedByExtensionName
 		sm.ExecutionID = callerState.ExecutionID
 	}
+	sm.Status = StatePREPROCESSING
 	errStates = sm.writeStates()
 	if errStates != nil {
 		log.Debug(errStates.Error())
@@ -1818,21 +1820,20 @@ func (sm *States) setStatesExecutionID(callerState *State) error {
 	return nil
 }
 
-func (sm *States) setExecutionTimes(isStart bool) error {
+func (sm *States) setExecutionTimesAndStatesStatus(status string) error {
 	errStates := sm.readStates()
 	if errStates != nil {
 		log.Debug(errStates.Error())
 		return errStates
 	}
 	timeNow := time.Now().UTC().Format(time.UnixDate)
-	if isStart {
+	if status == StateRUNNING {
 		sm.StartTime = timeNow
 		sm.EndTime = ""
-		sm.Status = StateRUNNING
 	} else {
 		sm.EndTime = timeNow
-		sm.Status = ""
 	}
+	sm.Status = status
 	errStates = sm.writeStates()
 	if errStates != nil {
 		log.Debug(errStates.Error())
@@ -1854,13 +1855,17 @@ func (sm *States) Execute(fromState string, toState string, callerState *State, 
 		log.Debug(err.Error())
 		return err
 	}
-	errStartTime := sm.setExecutionTimes(true)
+	errStartTime := sm.setExecutionTimesAndStatesStatus(StateRUNNING)
 	if errStartTime != nil {
 		log.Debug(errStartTime.Error())
 		return errStartTime
 	}
 	err = sm.executeStates(fromState, toState, callerState, callerOutFile)
-	errStopTime := sm.setExecutionTimes(false)
+	status := StateSUCCEEDED
+	if err != nil {
+		status = StateFAILED
+	}
+	errStopTime := sm.setExecutionTimesAndStatesStatus(status)
 	if errStopTime != nil {
 		log.Debug(errStopTime.Error())
 		return errStopTime
